651f858ecedef37abf02f42a455156777025108b
Jeff Gilfelt <jgilfelt@gmail.com>
2014-11-02 19:59:51 +0000

    distribute

--- js/asset-studio.js
+++ js/asset-studio.js
@@ -2611,6 +2611,61 @@ imagelib.toDataUri = function(img) {
   return canvas.toDataURL();
 };
 
+
+imagelib.longshadow = {};
+
+imagelib.longshadow.setColor = function(imgData, x, y, color) {
+  var index = (y * imgData.width + x) * 4;
+  var data = imgData.data;
+  data[index] = color[0];
+  data[index + 1] = color[1];
+  data[index + 2] = color[2];
+  data[index + 3] = color[3];
+};
+
+imagelib.longshadow.getAlpha = function(imgData, x, y) {
+  var data = imgData.data;
+  var index = (y * imgData.width + x) * 4 + 3;
+  return data[index];
+};
+
+imagelib.longshadow.isInShade = function(imgData, x, y) {
+  var data = imgData.data;
+  while (true) {
+    // traverse towards top/left
+    x -= 1;
+    y -= 1;
+    if (x < 0 || y < 0) {
+      // reached edge
+      return false;
+    }
+    if (imagelib.longshadow.getAlpha(imgData, x, y)) {
+      // alpha value casts shade
+      return true;
+    }
+  }
+};
+
+imagelib.longshadow.castShade = function(imgData, x, y) {
+  var n = 32;
+  var step = n / (imgData.width + imgData.height);
+  var alpha = n - ((x + y) * step);
+  var color = [0, 0, 0, alpha];
+  return imagelib.longshadow.setColor(imgData, x, y, color);
+};
+
+imagelib.longshadow.render = function(ctx, w, h) {
+  var imgData = ctx.getImageData(0, 0, w, h);
+  for(var y = 0; y < imgData.height; y++) {
+    for(var x = 0; x < imgData.width; x++) {
+      if (imagelib.longshadow.isInShade(imgData, x, y)) {
+        imagelib.longshadow.castShade(imgData, x, y);
+      }
+    }
+  }
+  ctx.putImageData(imgData, 0, 0);
+};
+
 imagelib.util = {};
 
 /**
